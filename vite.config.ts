09bc6834160c5f23e54b3dd65cff35e4f976b692
Emile Fugulin <code@efugulin.com>
2024-04-26 18:21:02 -0400

    Add new format

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import zipPack from "vite-plugin-zip-pack";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: "manifest.json",
+          dest: "../",
+        },
+      ],
+    }),
+    zipPack({
+      outFileName: "plugin.zip",
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
@@ -10,7 +25,7 @@ export default defineConfig({
       fileName: (format) => "script.js",
       formats: ["es"],
     },
-    outDir: "dist",
+    outDir: "dist/frontend",
     rollupOptions: {
       output: {
         manualChunks: undefined,
