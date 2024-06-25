3c86eb20e35a081c376e29c217d1a1d3abc39ae7
Emile Fugulin <code@efugulin.com>
2024-06-25 09:44:09 -0400

    Base pnpm workspace

---
+++ packages/backend/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "plugin-template-backend",
+      fileName: (format) => "script.js",
+      formats: ["es"],
+    },
+    outDir: "../../dist/backend",
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 import zipPack from "vite-plugin-zip-pack";
-import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,18 +17,4 @@ export default defineConfig({
       outFileName: "plugin.zip",
     }),
   ],
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "plugin-template",
-      fileName: (format) => "script.js",
-      formats: ["es"],
-    },
-    outDir: "dist/frontend",
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
 });
